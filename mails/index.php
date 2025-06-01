0551dbb27dbe00a299207d4b808c1a166317208a
Valentin Chmara <valentinchmara@gmail.com>
2025-06-01 20:18:54 +0200

    fix: remaining index.php

---
+++ mails/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * MIT License
+ * Copyright (c) 2025 Valentin Chmara
+ *
+ * @author Valentin Chmara
+ * @copyright Valentin Chmara
+ * @license MIT
+ */
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
